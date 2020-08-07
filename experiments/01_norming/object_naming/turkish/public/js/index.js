8266e926badb07ac819c8af85b4a4d7f77fd1c4a
Leyla <lkursat@stanford.edu>
2020-08-07 06:12:57 -0700

    fixed typo

--- experiments/01_norming/object_naming/turkish/public/js/index.js
+++ experiments/01_norming/object_naming/turkish/public/js/index.js
@@ -92,7 +92,7 @@ function make_slides(f) {
 
 	    this.stim = stim;
 	    //console.log(this.stim);
-	    var contextsentence = "Bu nesne nedir?<br>s<small><b>(Lütfen Türkçe cevap verin)" ;
+	    var contextsentence = "Bu nesne nedir?<br><small><b>(Lütfen Türkçe cevap verin)</b></small>" ;
 	    var objimagehtml = '<img src="images/'+stim.label+'.png" style="height:230px;">';
 
 	    $("#contextsentence").html(contextsentence);
